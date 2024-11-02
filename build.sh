b174c9743d96ddf8879448e1c13435776033e080
Mengye Ren <renmengye@gmail.com>
2024-11-02 13:41:07 -0400

    margin

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ set -e # exit with nonzero exit code if anything fails
 
 npm run build-in-place
 
-mkdir -p out/styles/
+find . -name ".DS_Store" -depth -exec rm {} \;
+rm -rf out
+mkdir -p out
 cp index.html index.js out/
 cp people.html out/
 cp research.html research.js out/
